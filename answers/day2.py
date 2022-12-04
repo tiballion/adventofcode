1c83fba9c4b55e2d65935f10eed22e7e2dd5bc21
tiballion <timothee.ballion.33@gmail.com>
2022-12-04 16:31:08 +0100

    day2.py part 1 finished

--- answers/day2.py
+++ answers/day2.py
@@ -3,10 +3,22 @@ import string
 with open('../inputs/day2.txt') as f:
     data = f.readlines()
 
-# Removes the newline character from the end of each line
-lines = [i.replace('\n', '') for i in data]
-score = 0
-for line in lines:
+
+def get_score(line):
     opp = string.ascii_uppercase.index(line.split(' ')[0]) + 1
     you = string.ascii_uppercase.index(line.split(' ')[1]) - 22
-    print(opp, you)
+    # Rock = 1
+    # Paper = 2
+    # Scissors = 3
+    if opp == you:
+        return you + 3
+    elif (opp == 1 and you == 3) or (opp == 2 and you == 1) or (opp == 3 and you == 2):
+        return you
+    else:
+        return you + 6
+
+
+# Removes the newline character from the end of each line
+lines = [i.replace('\n', '') for i in data]
+score = sum([get_score(i) for i in lines])
+print(score)
